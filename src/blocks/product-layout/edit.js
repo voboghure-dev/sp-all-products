848f1952b306f4d93c73da905720c384327cf362
Tapan Kumer Das <voboghure.dev@gmail.com>
2022-02-22 17:41:53 +0600

    Add InspectorControls with reange and text control
    Signed-off-by: Tapan Kumer Das <voboghure.dev@gmail.com>

--- src/blocks/product-layout/edit.js
+++ src/blocks/product-layout/edit.js
@@ -1,31 +1,104 @@
 import { __ } from "@wordpress/i18n";
-import { useBlockProps } from "@wordpress/block-editor";
+import {
+	ContrastChecker,
+	InspectorControls,
+	PanelColorSettings,
+	RichText,
+	useBlockProps,
+} from "@wordpress/block-editor";
+import {
+	Panel,
+	PanelBody,
+	TextControl,
+	ToggleControl,
+	SelectControl,
+	RangeControl,
+} from "@wordpress/components";
 import "./editor.scss";
 
-import shoe from './shoe.png';
+import shoe from "./shoe.png";
 
-export default function Edit() {
+export default function Edit({ attributes, setAttributes }) {
+	const { gridColumns, gridRows, gridGap } = attributes;
+
 	return (
-		<div {...useBlockProps()}>
-			<div className="wrapper">
-				<div className="card">
-					<div className="arivalDate">Arived in Dec 27</div>
-					<div className="imageArea">
-						<img src={shoe} alt="" />
-					</div>
-					<div className="discount">59% Off</div>
-					<div className="productName">Men's Annapolis Desert Lather Chukka</div>
-					<div className="itemPrice">
-						<span>$94 </span>$50
-					</div>
-					<div className="colorCercle">
-						<span className="cercle"></span>
-						<span className="cercle red"></span>
-						<span className="cercle green"></span>
-						<span className="cercle blue"></span>
+		<>
+			<InspectorControls>
+				<Panel>
+					<PanelBody
+						title={__("Column & Row", "sp-all-products")}
+						icon="grid-view"
+					>
+						<RangeControl
+							label="Columns"
+							value={gridColumns}
+							onChange={(gridColumns) => setAttributes({ gridColumns })}
+							min={1}
+							max={12}
+						/>
+						<RangeControl
+							label="Rows"
+							value={gridRows}
+							onChange={(gridRows) => setAttributes({ gridRows })}
+							min={1}
+							max={10}
+						/>
+						<TextControl
+							label={__("Column spacing", "sp-all-products")}
+							help={__("Write only number without px.", "sp-all-products")}
+							onChange={(gridGap) => setAttributes({ gridGap })}
+							value={gridGap}
+						/>
+					</PanelBody>
+					{/* <PanelColorSettings
+						title={__("Color Settings", "sp-all-products")}
+						icon="art"
+						initialOpen={false}
+						colorSettings={[
+							{
+								value: blockColor,
+								onChange: (blockColor) => setAttributes({ blockColor }),
+								label: __("Font Color", "sp-all-products"),
+							},
+							{
+								value: blockBackground,
+								onChange: (blockBackground) =>
+									setAttributes({ blockBackground }),
+								label: __("Background Color", "sp-all-products"),
+							},
+						]}
+					>
+						<ContrastChecker
+							isLargeText="false"
+							textColor={blockColor}
+							backgroundColor={blockBackground}
+						/>
+					</PanelColorSettings> */}
+				</Panel>
+			</InspectorControls>
+			<div {...useBlockProps()}>
+				<div className="wrapper">
+					<div className="card">
+						<div className="arivalDate">Arived in Dec 27</div>
+						<div className="imageArea">
+							<img src={shoe} alt="" />
+						</div>
+						<div className="discount">59% Off</div>
+						<div className="productName">
+							Men's Annapolis Desert Lather Chukka
+						</div>
+						<div className="itemPrice">
+							<span>$94 </span>$50
+						</div>
+						<div className="colorCercle">
+							<span className="cercle"></span>
+							<span className="cercle red"></span>
+							<span className="cercle green"></span>
+							<span className="cercle blue"></span>
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
+		</>
 	);
 }
