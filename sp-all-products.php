0755632919edfbcfe0cbe0bd7170cc36ac14aa20
Tapan Kumer Das <voboghure.dev@gmail.com>
2022-03-21 17:07:40 +0600

    Implement all product list block
    Signed-off-by: Tapan Kumer Das <voboghure.dev@gmail.com>

--- sp-all-products.php
+++ sp-all-products.php
@@ -151,6 +151,97 @@ function render_callback_product_grid( $attributes, $content ) {
 }
 
 /**
+ * Render callback for product grid
+ */
+function render_callback_product_list( $attributes, $content ) {
+  // Grid column
+  $columns = (int) $attributes['gridColumns'];
+
+  // Number of product (limit = Columns * Rows)
+  $limit = (int) $attributes['gridColumns'] * (int) $attributes['gridRows'];
+
+  // Grid gap by pixel
+  $gridGap = $attributes['gridGap'] . 'px;';
+
+  // Product categories
+  $tax_query = '';
+  if ( isset( $attributes['productCategories'] ) ) {
+    $productCategories = json_decode( $attributes['productCategories'] );
+    if ( ! empty( $productCategories ) ) {
+      foreach ( $productCategories as $cat ) {
+        $cat_id[] = $cat->value;
+      }
+      $tax_query = [
+        [
+          'taxonomy' => 'product_cat',
+          'field'    => 'term_id',
+          'terms'    => $cat_id,
+          'operator' => 'IN',
+        ],
+      ];
+    }
+  }
+
+  // Product offset
+  $offset = $attributes['productOffset'];
+
+  // Product order by
+  $order_by = $attributes['productOrderBy'];
+
+  // Product order
+  $order = $attributes['productOrder'];
+
+  // Get products using arguments
+  $args = [
+    'tax_query' => is_array( $tax_query ) ? $tax_query : null,
+    'offset'    => $offset,
+    'limit'     => $limit ? $limit : 0,
+    'orderby'   => $order_by ? $order_by : 'title',
+    'order'     => $order ? $order : 'ASC',
+  ];
+
+  $products = wc_get_products( $args );
+
+  ob_start();
+  echo '<div class="sp-container" style="--item-size: ' . $columns . '; --grid-gap: ' . $gridGap . '">';
+  echo '<ul class="sp-product-view-list">';
+  global $product;
+  foreach ( $products as $product ) {
+    echo '<li class="sp-products__item">';
+    // <span class="sp-sales">Sales</span> For sales tag
+
+		echo '<div class="sp-product-img">';
+    echo $product->get_image( 'woocommerce_thumbnail' );
+		echo '</div>';
+
+		echo '<div class="sp-wrapper-content">';
+		echo '<div class="content">';
+
+    if ( $attributes['toggleTitle'] ) {
+      echo '<h3 class="sp-product-title">' . $product->get_name() . '</h3>';
+    }
+
+    if ( $attributes['toggleDescription'] ) {
+      echo '<p>' . $product->get_short_description() . '</p>';
+    }
+
+    if ( $attributes['togglePrice'] ) {
+      echo '<h3 class="sp-product-price">' . $product->get_price_html() . '</h3>';
+    }
+
+    if ( $attributes['toggleAddToCart'] ) {
+      echo '<div class="sp-add-to-card"><a href="#">Add to Card</a></div>';
+    }
+		echo '</div>';
+		echo '</div>';
+    echo '</li>';
+  }
+  echo '</ul>';
+  echo '</div>';
+  return ob_get_clean();
+}
+
+/**
  * Debug log function to view any data in wp-content/debug.log
  * Uses: log_it($variable);
  */
